05e40c2962b60c8ad1f120baa8602ed10c10af03
bernst4315 <bernst4315@gmail.com>
2025-01-15 20:07:12 -0700

    able to fetch card info from api

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 console.log("hello")
+
+async function getPokeCard(){
+    const response = await fetch(`https://api.pokemontcg.io/v2/cards/`)
+    const jsonData = await response.json(); 
+    console.log(jsonData.data[159]);
+}
+
+getPokeCard();
