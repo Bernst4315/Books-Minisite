3a99222212aac7076cd887d0129a842d3ea51799
bernst4315 <bernst4315@gmail.com>
2025-01-16 20:39:51 -0700

    working on create shop item function
    commented out code

--- script.js
+++ script.js
@@ -1,24 +1,47 @@
-const pkmnCardImg1 = document.getElementById("img1")
-const pkmnCardImg2 = document.getElementById("img2")
-const pkmnCardImg3 = document.getElementById("img3")
-const pkmnCards =[]; 
+const shop = document.getElementById("shop");
 
-async function getPokeCard(){
-    const response = await fetch(`https://api.pokemontcg.io/v2/cards/`)
-    const jsonData = await response.json(); //remember jsonData has all the arrays of cards. could be used somehow for a search feature
-    pkmnCards.push(jsonData.data[223].images.small)
-    pkmnCards.push(jsonData.data[244].images.small)
-    pkmnCards.push(jsonData.data[137].images.small)
-    
-    pkmnCardImg1.src = pkmnCards[0];
-    pkmnCardImg2.src = pkmnCards[1];
-    pkmnCardImg3.src = pkmnCards[2];
-
-    console.log(jsonData.data);
-    
+function createShopItem(){
+    document.
 }
 
-getPokeCard();
+
+
+
+// const pkmnCardImg1 = document.getElementById("img1")
+// const pkmnCardImg2 = document.getElementById("img2")
+// const pkmnCardImg3 = document.getElementById("img3")
+
+// const Card1 = document.getElementById("card1")
+// const Card2 = document.getElementById("card2")
+// const Card3 = document.getElementById("card3")
+
+// const pkmnCards =[]; 
+
+// async function getPokeCard(){
+//     const response = await fetch(`https://api.pokemontcg.io/v2/cards/`)
+//     const jsonData = await response.json(); //remember jsonData has all the arrays of cards. could be used somehow for a search feature
+
+//     pkmnCards.push(jsonData.data[223])
+//     pkmnCards.push(jsonData.data[244])
+//     pkmnCards.push(jsonData.data[137])
+    
+//     pkmnCardImg1.src = pkmnCards[0].images.small;
+//     pkmnCardImg2.src = pkmnCards[1].images.small;
+//     pkmnCardImg3.src = pkmnCards[2].images.small;
+
+//     const text = document.createElement("p")
+//     text = pkmnCards[0].flavorText;
+//     card1.appendChild(text);
+//     // card2.appendChild(pkmnCards[1].flavorText);
+//     // card3.appendChild(pkmnCards[2].flavorText);
+
+//     console.log(jsonData.data[223].flavorText);
+    
+// }
+
+// getPokeCard();
+
+
 //let url = pkmnCards[0];
 //console.log(url);
 
