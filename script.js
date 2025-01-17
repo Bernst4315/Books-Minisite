854404425b421dd939957189386f48dd1bd28eed
bernst4315 <bernst4315@gmail.com>
2025-01-16 21:00:08 -0700

    finished createShopIItem function

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
 const shop = document.getElementById("shop");
 
-function createShopItem(){
-    document.
+function createShopItem(num = 1){
+    
+    for(let i = 0; i < num; i++){
+        const shopItem = document.createElement("div");
+        const image = document.createElement("img");
+        const text = document.createElement("p");
+        const price = document.createElement("h4");
+        const button = document.createElement("button");
+
+        text.textContent = "Insert text here";
+        price.textContent = "$$$"
+        button.textContent = "Buy"
+        image.alt = "Image of a pokemon card"
+        shopItem.className = "pkmnCard";
+        shopItem.id = `${i + 1}`; 
+
+        shop.appendChild(shopItem);
+        shopItem.appendChild(image);
+        shopItem.appendChild(text);
+        shopItem.appendChild(price)
+        shopItem.appendChild(button)
+    }
 }
 
-
+createShopItem(3);
 
 
 // const pkmnCardImg1 = document.getElementById("img1")
