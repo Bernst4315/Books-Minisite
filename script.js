c39a8260072ae44f0c7fb2bfe34fa0bebb590b97
bernst4315 <bernst4315@gmail.com>
2025-01-16 19:55:28 -0700

    added colors to website. Most beautiful webpage ever!

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-console.log("hello")
+const pkmnCardImg = document.getElementById("img1")
 
 async function getPokeCard(){
     const response = await fetch(`https://api.pokemontcg.io/v2/cards/`)
-    const jsonData = await response.json(); 
+    const jsonData = await response.json(); //remember jsonData has all the arrays of cards. could be used somehow for a search feature
     console.log(jsonData.data[159]);
 }
+
+
 
 getPokeCard();
