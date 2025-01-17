da97ef251397452386849b879989987fa501802f
bernst4315 <bernst4315@gmail.com>
2025-01-16 20:19:07 -0700

    got cards images populated

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
-const pkmnCardImg = document.getElementById("img1")
+const pkmnCardImg1 = document.getElementById("img1")
+const pkmnCardImg2 = document.getElementById("img2")
+const pkmnCardImg3 = document.getElementById("img3")
+const pkmnCards =[]; 
 
 async function getPokeCard(){
     const response = await fetch(`https://api.pokemontcg.io/v2/cards/`)
     const jsonData = await response.json(); //remember jsonData has all the arrays of cards. could be used somehow for a search feature
-    console.log(jsonData.data[159]);
+    pkmnCards.push(jsonData.data[223].images.small)
+    pkmnCards.push(jsonData.data[244].images.small)
+    pkmnCards.push(jsonData.data[137].images.small)
+    
+    pkmnCardImg1.src = pkmnCards[0];
+    pkmnCardImg2.src = pkmnCards[1];
+    pkmnCardImg3.src = pkmnCards[2];
+
+    console.log(jsonData.data);
+    
 }
 
-
-
 getPokeCard();
+//let url = pkmnCards[0];
+//console.log(url);
+
+
