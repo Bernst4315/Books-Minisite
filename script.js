dcfff80ea30186c2beedda610edd31d1d220c474
bernst4315 <bernst4315@gmail.com>
2025-01-17 20:32:41 -0700

    attempted to make search feature

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const shop = document.getElementById("shop");
+const searchResults = document.getElementById("search");
+const searchBtn = document.getElementById("search-btn");
 let num = 0;
 
 function createShopItem(num = 1){
@@ -32,17 +34,18 @@ function createShopItem(num = 1){
 }
 
 let totalNum = createShopItem(3);
-
 const pkmnCards =[]; 
+const allPkmnCards = getPokeCard();
 
 async function getPokeCard(){
     const response = await fetch(`https://api.pokemontcg.io/v2/cards/`)
     const jsonData = await response.json(); //remember jsonData has all the arrays of cards. could be used somehow for a search feature
+    const allPkmnCards = jsonData.data;
 
     pkmnCards.push(jsonData.data[223])
     pkmnCards.push(jsonData.data[244])
     pkmnCards.push(jsonData.data[137])
-     
+    
     console.log(jsonData.data);
     console.log(pkmnCards);
     
@@ -55,9 +58,47 @@ async function getPokeCard(){
         cardText.textContent = card.flavorText;
         cardPrice.textContent = `$${card.cardmarket.prices.averageSellPrice}`; 
     })
+
+    
+    let text = document.createElement("p");
+    text.textContent = jsonData.data[223].types[0];
+    searchResults.appendChild(text);
+
+    //searchByType(allPkmnCards, "Grass")  
+    console.log(`type ${jsonData.data[223].types[0]}`)
+    
+    return jsonData.data;
+    
     
 }
 
+searchBtn.addEventListener("click", ()=> {
+    console.log(allPkmnCards);
+    // allPkmnCards.forEach((card) => {
+    //     if(card.types[0] === "Grass"){
+    //         let text = document.createElement("p");
+    //         text.textContent = card.types[0];
+    //         searchResults.appendChild(text);
+    //     }
+    // }) //this block of code is trying to loop through array of obj to grab all the ones that have the searched for type
+    //(currently hardcoded) and make a list of results in the HTML
+    //would eventually try to make it work via user input
+})
+
+
+// async function searchByType(cards, type){
+    
+//     cards.forEach((card) => {
+//         if(card.types[0] === type){
+//             let text = document.createElement("p");
+//             text.textContent = cards.name;
+//             searchResults.appendChild(text)
+//         }
+//     })   
+    
+// }
+
+ 
 getPokeCard();
 console.log(pkmnCards);
 console.log(totalNum);
